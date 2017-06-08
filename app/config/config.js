e5ac71012eb0039f53ff71652d7bf4368c5efcc5
Brian Mullis <brian@MACINTOSH.local>
2017-06-08 10:57:23 -0700

    initial commit

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,4 @@
 angular.module("config", [])
-.constant("BUCKET_SLUG", "events-app")
-.constant("MEDIA_URL", "https://api.cosmicjs.com/v1/events-app/media")
-.constant("READ_KEY", "")
-.constant("WRITE_KEY", "");
+.constant("MEDIA_URL", "https://api.cosmicjs.com/v1/first-sample-project/media")
+.constant("READ_KEY", "qMfNZvZ744LFE7sjHa4Icz5QCGZghRszpOVJIEfQ9CEqRWEkAI")
+.constant("WRITE_KEY", "e3q5GLn6HTlqIPRMgHcaD55jbeqOHhz0e00bL6lEaLNpRd02uh");
--- dist/js/main.js
+++ dist/js/main.js
@@ -315,8 +315,7 @@
         });  
 })();  
 angular.module("config", [])
-.constant("BUCKET_SLUG", "events-app")
-.constant("MEDIA_URL", "https://api.cosmicjs.com/v1/events-app/media")
+.constant("MEDIA_URL", "https://api.cosmicjs.com/v1/undefined/media")
 .constant("READ_KEY", "")
 .constant("WRITE_KEY", "");
 
